c6cbf06891bee802688a983cd28d519134f321f7
meet suva <meetsuva10@gmail.com>
2023-11-08 23:10:25 +0530

    fetch data from blogs

--- app/model/model.php
+++ app/model/model.php
@@ -1,19 +1,39 @@
 <?php
-class Database{
-    private $servername = "localhost";
-    private $username = "root";
-    private $password = "";
-    private $database = "urbansoft";
-    private $connection;
+class Database
+{
+    protected $servername = "localhost";
+    protected $username = "root";
+    protected $password = "";
+    protected $database = "urbansoft";
+    protected $connection;
 
     function __construct()
     {
-        try{
-            $this->connection = new mysqli($this->servername,$this->username,$this->password,$this->database);
-        }
-        catch(Exception $ex){
-            echo "Database is not connected".$ex->getMessage();
+        try {
+            $this->connection = new mysqli($this->servername, $this->username, $this->password, $this->database);
+        } catch (Exception $ex) {
+            echo "Database is not connected" . $ex->getMessage();
         }
     }
 }
-?>
+
+class getContentGrid extends Database
+{
+    function __construct()
+    {
+        parent::__construct();
+        $queryFire = $this->connection->query("select blog_id,blog_title,blog_image,blog_meta from blogs");
+
+        if ($queryFire->num_rows) {
+            while ($fetchData = $queryFire->fetch_assoc()) {
+                return $dataBundle = [
+                    "id" => $fetchData['blog_id'],
+                    "title" => $fetchData['blog_title'],
+                    "meta" => $fetchData['blog_meta'],
+                    "image" => $fetchData['blog_image']
+                ];
+            }
+        } else {
+        }
+    }
+}
--- app/view/blogs.php
+++ app/view/blogs.php
@@ -1,3 +1,57 @@
-<?php
-echo "<h1>this is blogs page</h1>";
-?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="public/css/header.css">
+    <link rel="stylesheet" href="public/css/footer.css">
+    <link rel="stylesheet" href="public/css/navigation.css">
+    <link rel="stylesheet" href="public/css/blogs.css">
+</head>
+
+<body>
+    <?php include "framework/header.php"; ?>
+    <main>
+        <div class="slide"></div>
+        <div class="content-grid">
+            <?php
+            $getContent = new getContentGrid();
+            $data = $getContent->__construct();
+            ?>
+            <a href="">
+                <div class="card">
+                    <h1><?php echo $data['title']; ?></h1>
+                    <img src="<?php echo $data['image']; ?>" alt="">
+                    <h2><?php echo $data['meta']; ?></h2>
+                </div>
+            </a>
+            <a href="">
+                <div class="card">
+                    <h1><?php echo $data['title']; ?></h1>
+                    <img src="<?php echo $data['image']; ?>" alt="">
+                    <h2><?php echo $data['meta']; ?></h2>
+                </div>
+            </a>
+            <a href="">
+                <div class="card">
+                    <h1><?php echo $data['title']; ?></h1>
+                    <img src="<?php echo $data['image']; ?>" alt="">
+                    <h2><?php echo $data['meta']; ?></h2>
+                </div>
+            </a>
+            <a href="">
+                <div class="card">
+                    <h1><?php echo $data['title']; ?></h1>
+                    <img src="<?php echo $data['image']; ?>" alt="">
+                    <h2><?php echo $data['meta']; ?></h2>
+                </div>
+            </a>
+        </div>
+    </main>
+    <?php include "framework/footer.php"; ?>
+    <?php include "framework/navigation.php"; ?>
+</body>
+
+</html>
